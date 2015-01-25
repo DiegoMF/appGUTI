40f229ab1dec07b21ee239050deee07e6c183a9a
DiegoMF <diego.marrujo@gmail.com>
2015-01-25 13:42:10 -0500

    clsConexion

--- SpringMVCTest/src/com/bcp/dao/Conexion.java
+++ SpringMVCTest/src/com/bcp/dao/Conexion.java
@@ -7,42 +7,39 @@ import com.google.appengine.api.utils.SystemProperty;
 
 public class Conexion {
 
-    public static Connection getConexion() throws Exception {
-        Connection con = null;
-        /*conexio en la Nube
-        String usuario = "root";
-        String clave = "123456";
-        String url = "jdbc:mysql://127.0.0.1:3306/dbtest";*/
-        
-        /*conexion local*/
-        String usuario = "root";
-        String clave = "root";
-        String url = "jdbc:mysql://127.0.0.1:3306/dbtest";
-        
-        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-        
-        if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
-        	  // Load the class that provides the new "jdbc:google:mysql://" prefix.
-        		usuario = "root";
-        	  clave = "";
-        	  Class.forName("com.mysql.jdbc.GoogleDriver");
-        	  url = "jdbc:google:mysql://everisgaetest:portafolio/BDPortafolio?useUnicode=true&characterEncoding=UTF-8";
-        	  
-        	  
-        	} else {
-        		
-        	  Class.forName("com.mysql.jdbc.Driver");
-        	  url = "jdbc:mysql://localhost:3306/bdportafolio1";
-//        	  url = "jdbc:mysql://7.227.104.181:8282/bdportafolio";
-        	  
+	public static Connection getConexion() throws Exception {
+		Connection con = null;
+		/*
+		 * conexio en la Nube String usuario = "root"; String clave = "123456";
+		 * String url = "jdbc:mysql://127.0.0.1:3306/dbtest";
+		 */
 
-        	}
-        //Class.forName("com.mysql.jdbc.Driver");
-        
-        con = DriverManager.getConnection(url, usuario, clave);
-        
+		/* conexion local */
+		String usuario = "root";
+		String clave = "root";
+		String url = "jdbc:mysql://127.0.0.1:3306/dbtest";
 
-        return con;
+		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 
-    }
+		if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+			// Load the class that provides the new "jdbc:google:mysql://"
+			// prefix.
+			usuario = "root";
+			clave = "";
+			Class.forName("com.mysql.jdbc.GoogleDriver");
+			url = "jdbc:google:mysql://everisgaetest:portafolio/BDPortafolio?useUnicode=true&characterEncoding=UTF-8";
+
+		} else {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			// url = "jdbc:mysql://localhost:3306/bdportafolio1";
+			url = "jdbc:mysql://173.194.230.96:3306/bdportafolio";
+
+		}
+
+		con = DriverManager.getConnection(url, usuario, clave);
+
+		return con;
+
+	}
 }
